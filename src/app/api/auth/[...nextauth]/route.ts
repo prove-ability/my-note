b52e6447d5dbcf8caa89b29e3ef7cad5fe2bd919
Bard <59461181+prove-ability@users.noreply.github.com>
2024-01-09 23:43:37 +0900

    chore: 코드 정리

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async signIn(userDetail: {}) {
+      if (Object.keys(userDetail).length === 0) {
+        return false;
+      }
+      return true;
+    },
+    async redirect({ baseUrl }: { baseUrl: string }) {
+      return `${baseUrl}/protected`;
+    },
+  }
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
